bc02f2bf915f9e61f1bd45e83bf72eeb2ade1048
I578057 <prasannakumar.v@sap.com>
2025-04-29 15:45:44 +0530

    Backend-Framework

---
+++ JudiciaryManagementSiteBackend/src/main/java/com/fsad/JudiciaryManagementSiteBackend/JudiciaryManagementSiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fsad.JudiciaryManagementSiteBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JudiciaryManagementSiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JudiciaryManagementSiteBackendApplication.class, args);
+	}
+
+}
---
+++ JudiciaryManagementSiteBackend/src/test/java/com/fsad/JudiciaryManagementSiteBackend/JudiciaryManagementSiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsad.JudiciaryManagementSiteBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JudiciaryManagementSiteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
